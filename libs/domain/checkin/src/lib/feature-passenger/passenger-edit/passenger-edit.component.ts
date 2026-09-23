1aa63901cdac2a62ab50b67b59f1f499950c22a8
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-09-23 16:08:20 +0100

    refactor: http resource

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -3,6 +3,8 @@ import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { PassengerService } from '../../logic-passenger/data-access/passenger.service';
 import { validatePassengerStatus } from '../../util-validation/passenger-validator/passenger-status.validator';
+import { httpResource } from '@angular/common/http';
+import { initialPassenger, Passenger } from '../../logic-passenger/model/passenger';
 
 
 @Component({
@@ -28,17 +30,23 @@ export class PassengerEditComponent {
   });
 
   readonly id = input(0, { transform: numberAttribute });
-  private readonly passengerResource = this.passengerService.findByIdAsResource(this.id); 
+  protected readonly passengerResource = httpResource<Passenger>(() => ({
+    url: 'https://demo.angulararchitects.io/api/passenger',
+    params: { id: this.id() }
+  }), {
+    defaultValue: initialPassenger
+  });
   
   constructor() {
     effect(() => {
       if (this.passengerResource.hasValue()) {
-        this.editForm.patchValue(this.passengerResource.value())
+        this.editForm.patchValue(this.passengerResource.value());
       }
     });
   }
 
   protected save(): void {
+    this.passengerResource.set(this.editForm.getRawValue());
     console.log(this.editForm.value);
   }
 }
