03402e016c8018954aaffb2b655bbb83e8cb59ba
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-09-23 14:04:23 +0100

    demo: take until destroyed, destroy ref

--- libs/domain/booking/src/lib/feature-flight/flight-edit/flight-edit.component.ts
+++ libs/domain/booking/src/lib/feature-flight/flight-edit/flight-edit.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, Input, OnChanges, OnInit, SimpleChanges, inject } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { routerFeature } from '@flight-demo/shared/state';
 import { Store } from '@ngrx/store';
 import { initialFlight } from '../../logic-flight/model/flight';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 
 @Component({
@@ -14,8 +15,9 @@ import { initialFlight } from '../../logic-flight/model/flight';
   ],
   templateUrl: './flight-edit.component.html'
 })
-export class FlightEditComponent implements OnChanges {
+export class FlightEditComponent implements OnInit, OnChanges {
   private store = inject(Store);
+  private destroyRef = inject(DestroyRef);
 
   @Input() flight = initialFlight;
 
@@ -28,7 +30,19 @@ export class FlightEditComponent implements OnChanges {
   });
 
   constructor() {
-    this.store.select(routerFeature.selectRouteParams).subscribe(
+    this.store.select(routerFeature.selectRouteParams).pipe(
+      takeUntilDestroyed()
+    ).subscribe(
+      params => console.log(params)
+    );
+
+    this.destroyRef.onDestroy(() => console.log('Bye, bye! :('));
+  }
+
+  ngOnInit(): void {
+    this.store.select(routerFeature.selectRouteParams).pipe(
+      takeUntilDestroyed(this.destroyRef)
+    ).subscribe(
       params => console.log(params)
     );
   }
