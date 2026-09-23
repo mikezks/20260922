0e4eef2f64f0552d11733b1891057c2637f40e1d
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-09-23 13:19:44 +0100

    refactor: lean rxjs interop

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -31,14 +31,11 @@ export class PassengerEditComponent {
   });
 
   readonly id = input(0, { transform: numberAttribute });
-  private readonly id$ = toObservable(this.id);
-  private readonly passenger$ = this.id$.pipe(
-    switchMap(id => this.passengerService.findById(id))
-  );
-  private readonly passenger = toSignal(this.passenger$, {
-    // requireSync: true
-    initialValue: initialPassenger
-  });
+  private readonly passenger = toSignal(
+    toObservable(this.id).pipe(
+      switchMap(id => this.passengerService.findById(id)
+    )
+  ), { initialValue: initialPassenger });
 
   constructor() {
     effect(() => this.editForm.patchValue(this.passenger()));
