4979596cbffe670a8e5e28a08484e33b46f2c1ea
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-09-24 10:21:23 +0100

    refactor: signal forms validation

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,10 +1,16 @@
 import { httpResource } from '@angular/common/http';
-import { ChangeDetectionStrategy, Component, effect, input, linkedSignal, numberAttribute, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, linkedSignal, numberAttribute } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { form, FormField } from '@angular/forms/signals';
+import { form, FormField, required, schema } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { initialPassenger, Passenger } from '../../logic-passenger/model/passenger';
 
+// (3) Field Logic: Validators, conditional disabled, hidden, readonly
+export const passengerSchema = schema<Passenger>(passengerPath => {
+  required(passengerPath.name, {
+    message: 'The Lastname is mandatory - please enter one.'
+  });
+});
 
 @Component({
   // eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection -- preserves the workshop's pre-v22 behavior
@@ -13,6 +19,7 @@ import { initialPassenger, Passenger } from '../../logic-passenger/model/passeng
   imports: [
     ReactiveFormsModule,
     RouterLink,
+    // (4) UI Control: Template Binding
     FormField
   ],
   templateUrl: './passenger-edit.component.html'
@@ -23,12 +30,16 @@ export class PassengerEditComponent {
     url: 'https://demo.angulararchitects.io/api/passenger',
     params: { id: this.id() }
   }), { defaultValue: initialPassenger });
+  // (1) Data Model: Writable Signal
   private passengerState = linkedSignal(() => this.passengerResource.hasValue()
     ? this.passengerResource.value()
     : initialPassenger
   , { set: passenger => this.passengerResource.set(passenger) });
-  protected readonly editForm = form(this.passengerState);
-
+  // (2): Field State: valid, touched, dirty, value, ...
+  protected readonly editForm = form(
+    this.passengerState,
+    passengerSchema
+  );
   
   protected save(): void {
     console.log(this.editForm().value());
